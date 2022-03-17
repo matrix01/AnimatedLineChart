9b92f417d041f2223c4b491bae1ad1c753d96f35
milan <milanaiub@gmail.com>
2022-03-18 00:16:49 +0900

    fix test

--- Tests/AnimatedLineChartTests/AnimatedLineChartTests.swift
+++ Tests/AnimatedLineChartTests/AnimatedLineChartTests.swift
@@ -6,6 +6,5 @@ final class AnimatedLineChartTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(AnimatedLineChart().text, "Hello, World!")
     }
 }
